32e9d0c859a05de50a51206af1bb949d0dfcad92
Tomas Vecchi <204607402+tvecchiUTN@users.noreply.github.com>
2025-09-08 11:59:48 -0300

    Actividad 2
    Se añadio a la funcion de la activdidad 1 la posibilidad de imprimir ciertos valores

---
+++ Act2/imprVec.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "imprVec.h"
+
+size_t print(void *base, size_t nitems, size_t size, int (*prt)(const void *), int (*cond)(const void *))
+{
+    size_t cuenta = 0;
+    if(cond == NULL)
+    {
+        char *puntero_base = (char *)base;
+
+        for(size_t i = 0; i < nitems; i++)
+        {
+            void *puntero_elemento = puntero_base + (i * size);
+            
+            prt(puntero_elemento);
+            cuenta++;
+        }
+    }
+    else
+    {
+        char *puntero_base = (char *)base;
+
+        for(size_t i = 0; i < nitems; i++)
+        {
+            void *puntero_elemento = puntero_base + (i * size);
+            
+            if(cond(puntero_elemento))
+            {
+                prt(puntero_elemento);
+                cuenta++;
+            }
+        }
+    }
+
+    return cuenta;
+}
+
+int printInt(const void *vec)
+{
+    printf("%d\n", *(int*)vec);
+    return 0;
+}
+
+int printFloat(const void *vec)
+{
+    printf("%.2f\n", *(float*)vec);
+    return 0;
+}
+
+int printMat(const void *vec)
+{
+    printf("%s\n", (char*)vec);
+    return 0;
+}
+
+int printString(const void *vec)
+{
+    printf("%s", (char*)vec);
+    printf("\n");
+    return 0;
+}
